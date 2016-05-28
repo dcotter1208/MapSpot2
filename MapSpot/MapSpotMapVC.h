99d0728d79008070579ffc78b25e8548985eb9d3
DetroitLabs <DetroitLabs@DonovanDLabsMacBookPro.local>
2016-05-28 16:10:03 -0400

    first commit. Map put on VC and .gitignore attached.

---
+++ MapSpot/MapSpotMapVC.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  MapSpot
+//
+//  Created by DetroitLabs on 5/27/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface MapSpotMapVC : UIViewController
+
+@property(nonatomic, strong) CLLocation *initialLocation;
+
+@end
+
---
+++ MapSpot/MapSpotMapVC.m
@@ -0,0 +1,42 @@
+//
+//  ViewController.m
+//  MapSpot
+//
+//  Created by DetroitLabs on 5/27/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+/*
+ 
+ MKAnnotationView.image is that image property that will return whatver image we want and not the pin. This is what we can set to the user's selected pin image.
+ 
+ */
+
+#import "MapSpotMapVC.h"
+
+@interface MapSpotMapVC ()
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+@end
+
+@implementation MapSpotMapVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void)mapSetup {
+    
+    
+    
+}
+
+@end
