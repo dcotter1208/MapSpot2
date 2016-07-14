f8238be588c87660f4196e14861364a081e5a36c
dcotter1208 <donovan.cotter@yahoo.com>
2016-07-14 14:41:17 -0400

    added MapAnnotationCallout UIView class with xib file

---
+++ MapSpot/MapAnnotationCallout.h
@@ -0,0 +1,15 @@
+//
+//  MapAnnotationCallout.h
+//  MapSpot
+//
+//  Created by DetroitLabs on 7/14/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapAnnotationCallout : UIView
+@property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *messageLabel;
+
+@end
---
+++ MapSpot/MapAnnotationCallout.m
@@ -0,0 +1,24 @@
+//
+//  MapAnnotationCallout.m
+//  MapSpot
+//
+//  Created by DetroitLabs on 7/14/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import "MapAnnotationCallout.h"
+
+@implementation MapAnnotationCallout
+
+
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
